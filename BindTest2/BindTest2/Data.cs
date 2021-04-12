c743f4b36dfa3e2a46ff7d0a79feb5fbd1669bda
bradshaw10 <eoghan.bradshaw@gmail.com>
2021-04-12 15:19:08 +0100

    Initial commit

---
+++ BindTest2/BindTest2/Data.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BindTest2
+{
+    public class Data : INotifyPropertyChanged
+    {
+        private string _dataText;
+        public string DataText
+        {
+            get
+            {
+                return _dataText;
+            }
+            set
+            {
+                _dataText = value;
+                if(PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("DataText"));
+
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
---
+++ BindTest2/BindTest2/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BindTest2
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            
+            InitializeComponent();
+            Data data = new Data
+            {
+                DataText = "Test1"
+            };
+            label.BindingContext = data;
+            label.SetBinding(Label.TextProperty, "DataText");
+
+            data.DataText = "Test2";
+        }
+
+        private void slider_Clicked(object sender, EventArgs e)
+        {
+            Data data = new Data();
+
+            data.DataText = "Test3";
+            label.BindingContext = data;
+            label.SetBinding(Label.TextProperty, "DataText");
+
+        }
+    }
+}
